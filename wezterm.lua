dfc901b7ea93c9fe9e4fd94832158c7f3f2b3a06
Md. Mehedi Hassan <mh.web.mehedihassan@gmail.com>
2024-07-25 12:49:37 +0600

    Update wezterm.lua file

--- wezterm.lua
+++ wezterm.lua
@@ -7,15 +7,29 @@ local config = {}
 -- config.leader = { key="space", mods="CTRL" }
 
 -- Color scheme
-config.color_scheme = 'TheMatrix'
+-- config.color_scheme = 'Dracula (Official)'
+
+config.colors = {
+	foreground = "#CBE0F0",
+	background = "#011423",
+	cursor_bg = "#47FF9C",
+	cursor_border = "#47FF9C",
+	cursor_fg = "#011423",
+	selection_bg = "#033259",
+	selection_fg = "#CBE0F0",
+	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+}
 
 -- Settings
 config.font_size = 13.0
 config.window_background_opacity = 0.85
--- config.window_decorations = "RESIZE"
+config.window_decorations = "RESIZE"
 config.window_close_confirmation = "AlwaysPrompt"
 config.scrollback_lines = 3000
 config.default_workspace = "main"
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
 
 -- Dim inactive panes
 config.inactive_pane_hsb = {
@@ -23,7 +37,7 @@ config.inactive_pane_hsb = {
   brightness = 0.5
 }
 -- Font settings
-config.font = wezterm.font('JetBrains Mono', { weight = 'Bold', italic = true })
+config.font = wezterm.font('JetBrains Mono', { weight = 'Bold', italic = false })
 
 -- Key bindings
 config.keys = {}
@@ -44,8 +58,6 @@ config.window_frame = {
   button_hover_fg = 'transparent',
   button_hover_bg = 'transparent',
 }
-
-config.tab_bar_at_bottom = true
 
 -- config.window_padding = {
 --   left = 0,
